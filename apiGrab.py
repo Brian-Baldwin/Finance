34b2331b58ca5f455576ec427adf8572718645cd
Brian-Baldwin <97992446+Brian-Baldwin@users.noreply.github.com>
2022-01-23 14:00:53 -0500

    Update apiGrab.py

--- apiGrab.py
+++ apiGrab.py
@@ -2,9 +2,8 @@ import requests
 import json
 
 
-def grab(ticker, date):
+def grab(ticker, date, api_key):
     base_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol='
-    api_key = '&apikey=UNESSNPYYAGT3RJJ'
     r = requests.get(base_url + ticker + api_key)
     data = r.text
     parse_json = json.loads(data)
